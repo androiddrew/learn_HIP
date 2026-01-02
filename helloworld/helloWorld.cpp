2871a69e74fad44ab5b92061dea122f4cc01be4b
Drew Bednar <drew@runcible.io>
2026-01-02 09:23:08 -0500

    Adding more comments, changing number of threads, and adding thread ids

--- helloworld/helloWorld.cpp
+++ helloworld/helloWorld.cpp
@@ -3,14 +3,24 @@
 // A GPU kernel must be prefixed with __global__to allow the compiler to generate
 // GPU specific code
 __global__ void gpuHello() {
-	printf("Hello GPU programming\n");
+	// This print inside a GPU kernel is a special convenience feature. The GPU
+	// is actually writing to a shared string buffer.
+	//
+	// The blockIdx.x and threadIdx.x only exist within the GPU context
+	printf("Hello GPU programming! Block: %d, Thread: %d\n", blockIdx.x, threadIdx.x);
 }
 
 int main(){
 	//<<<1,1>>> here is inserted between the function name and the parameter list.
-	//this denotes the number of threads within the kernel. Here we are creating
-	//a single thread in the kernel
-	gpuHello<<<1,1>>>();
+	//This denotes a Grid number and a block number. Here we are creating
+	//a single thread in the kernel by creating one grid with a block with one thread.
+	//You could change that to <<<2,6>>> and get 2 blocks each with 6 threads. Each thread
+	//executing it's own instance of the kernel.
+	gpuHello<<<4,4>>>();
+	//
+	//gpuHello<<<2,6>>>();
+	//Tells the CPU to wait until the GPU is done. At this point ROCm runtime flushes
+	// the shared GPU string buffer to stdout
 	hipDeviceSynchronize();
 }
 
